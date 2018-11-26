2b7099938af959da8c09945d7245766c52e9eac6
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-26 14:46:28 +0000

    Display all topic titles and style topics

--- src/Components/Topics.jsx
+++ src/Components/Topics.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { getTopics } from '../api';
+import '../Topics.css';
 
 class Topics extends Component {
+  state = {
+    topics: [],
+    loading: true
+  }
+
   render() {
     return (
-      <div className='parent'>
-        <header>
-          <h1>Topics</h1>
-        </header>
-        <p>Topic 1</p>
-        <p>Topic 2</p>
-      </div>
+      this.state.loading ? <p>Loading...</p> :
+        <div className='parent'>
+          <header>
+            <h1>Topics</h1>
+          </header>
+          <ul>
+            {this.state.topics.map(topic => {
+              return <li>{topic.title}</li>
+            })}
+          </ul>
+        </div>
     );
+  }
+
+  componentDidMount() {
+    getTopics()
+      .then(topics => {
+        this.setState({
+          topics,
+          loading: false
+        })
+      })
   }
 }
 
--- src/api.js
+++ src/api.js
@@ -10,3 +10,8 @@ export const getArticle = async (id) => {
   const { data } = await axios.get(`${BASE_URL}/articles/${id}`);
   return data.article;
 }
+
+export const getTopics = async () => {
+  const { data } = await axios.get(`${BASE_URL}/topics`);
+  return data.topics;
+}
