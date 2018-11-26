cc58c2a4e9585e964cc23bb89f5ab7c369ba757f
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-26 12:01:51 +0000

    Set up basic Router paths

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './Components/Nav';
 import Articles from './Components/Articles';
+import Article from './Components/Article';
+import Comments from './Components/Comments';
+import User from './Components/User';
 import { Router } from '@reach/router';
 
 class App extends Component {
@@ -13,6 +16,13 @@ class App extends Component {
         </header>
         <Nav />
         <Articles />
+        <Router>
+          <Home path='/' />
+          <Home path='/articles' />
+          <Article path='/articles/:article_id' />
+          <Comments path='/comments/:comment_id' />
+          <User path='/users/username' />
+        </Router>
         <footer>Footer</footer>
       </div>
     );
