149dac03ecf11aceda652c39edd65e750a5ba573
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-26 11:49:56 +0000

    Basic CSS grid set up for homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './Components/Nav';
-import {Router} from '@reach/router';
+import Articles from './Components/Articles';
+import { Router } from '@reach/router';
 
 class App extends Component {
   render() {
     return (
-      <div >
+      <div className="App">
         <header>
           <h1>Homepage</h1>
         </header>
         <Nav />
-        
+        <Articles />
       </div>
     );
   }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div>
-      
+    <div className="nav">
+      <nav></nav>
     </div>
   );
 };
