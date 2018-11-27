63eeb4b00899972b270df1463a3848251d7ea483
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-27 15:23:09 +0000

    Formatting article adding and trying to format homepage layout

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -3,21 +3,23 @@ import { Link } from '@reach/router';
 
 const Articles = ({ articles, newArticle, newArticleTitle, handleSubmit, handleChange, handleChangeTitle, changeTopic, topic }) => {
   return (
-    <div className='articles'>
-      <select onChange={changeTopic} defaultValue=''>
-        <option value={topic}>Choose a topic...</option>
-        <option value='coding'>Coding</option>
-        <option value='football'>Football</option>
-        <option value='cooking'>Cooking</option>
-      </select>
+    <div>
+      <form className='add-article'>
+        <select onChange={changeTopic} defaultValue=''>
+          <option value={topic}>Choose a topic...</option>
+          <option value='coding'>Coding</option>
+          <option value='football'>Football</option>
+          <option value='cooking'>Cooking</option>
+        </select>
 
-      <input type='text' placeholder='Add an article title...' onChange={handleChangeTitle} value={newArticleTitle} />
+        <input type='text' placeholder='Add an article title...' onChange={handleChangeTitle} value={newArticleTitle} />
 
-      <input type='text' placeholder='Add an article...' onChange={handleChange} value={newArticle} />
+        <input type='text' placeholder='Add an article...' onChange={handleChange} value={newArticle} />
 
-      <button onClick={handleSubmit}>Add an article</button>
+        <button onClick={handleSubmit}>Add an article</button>
+      </form>
 
-      <ul>
+      <ul className='articles'>
         {articles.map(article => {
           let date = new Date(article.created_at)
           let day = date.getDay()
