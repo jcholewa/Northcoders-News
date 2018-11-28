de7474734ca8da169b1ebed776095d6786e768a6
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-28 17:15:02 +0000

    Logged in data positioning

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -12,24 +12,24 @@ class Nav extends Component {
   render() {
     return (
       this.state.loading ? <p>Loading...</p> :
-      <>
-        <div className="nav">
-          <nav>
-            <Link to='/'>Home</Link>
-            {'     |     '}
-            {this.state.topics.map((topic, index) => {
-              if (index !== this.state.topics.length - 1)
-                return <Link to={`/topics/${topic.slug}/articles`} key={index}>{topic.title}
-                  {'     |     '} </Link>
-              else return <Link to={`/topics/${topic.slug}/articles`} key={index}>{topic.title} </Link>
-            })}
-          </nav>
-        </div>
-        {this.props.user.username ? <div className='logged-in'>
-        <p>Current user: {this.props.user.username}</p>
-        <button onClick={this.props.userLogout}>Log Out</button>
-      </div> : <> </>}
-      </>
+        <>
+          <div className="nav">
+            <nav>
+              <Link to='/'>Home</Link>
+              {'     |     '}
+              {this.state.topics.map((topic, index) => {
+                if (index !== this.state.topics.length - 1)
+                  return <Link to={`/topics/${topic.slug}/articles`} key={index}>{topic.title}
+                    {'     |     '} </Link>
+                else return <Link to={`/topics/${topic.slug}/articles`} key={index}>{topic.title} </Link>
+              })}
+            </nav>
+          </div>
+          {this.props.user.username ? <div className='logged-in'>
+            <p>Current user: <Link to={`/users/${this.props.user.username}`}>{this.props.user.username}</Link></p>
+            <button onClick={this.props.userLogout}>Log Out</button>
+          </div> : <> </>}
+        </>
     );
   }
 
