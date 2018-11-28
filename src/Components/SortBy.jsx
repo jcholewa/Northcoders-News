2dc36f9ffd1000f5222d6c349fc939c00bc95858
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-28 09:56:59 +0000

    Basic sort by dropdown set up

---
+++ src/Components/SortBy.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SortBy = () => {
+  return (
+    <div>
+      <form >
+        <select >
+          <option>Choose an option...</option>
+          <option>Number of votes</option>
+          <option>Time posted</option>
+        </select>
+      </form>
+    </div>
+  );
+};
+
+export default SortBy;
