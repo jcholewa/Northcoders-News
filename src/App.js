2050b7b36898d5451668f6ad490ebd522cb941d3
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-28 16:29:29 +0000

    Troubleshooting over articles by topic not appearing (due to typo)

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
             <Router>
               <Home path='/' user={this.state.user} />
               <Home path='/articles' user={this.state.user} />
-              <Home path='/topics/:topic_slug/User' />
+              <Home path='/topics/:topic_slug/articles' />
               <Article path='/articles/:article_id/*' user={this.state.user} />
               <User path='/users/:username' />
             </Router>
