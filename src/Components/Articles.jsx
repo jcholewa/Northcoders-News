d74236db80f6a60b4a0ede11af0ce7c4fab2f4e8
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-28 11:45:56 +0000

    Add loading ternary to Articles component

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -32,26 +32,27 @@ class Articles extends Component {
           <button onClick={this.handleSubmit}>Add an article</button>
         </form>
 
-        <ul className='articles'>
-          {this.state.articles.map(article => {
-            let date = new Date(article.created_at)
-            let day = date.getDay()
-            let month = date.getMonth()
-            let year = date.getFullYear()
-            {
-              if (this.state.loading) return <p>Loading...</p>
-              return (
-                <li key={article._id}>
-                  <Link to={`/articles/${article._id}`}>{article.title}</Link>
-                  <p>by {article.created_by.name}</p>
-                  <p>Posted on: {day}/{month}/{year}</p>
-                  <p>{article.body.substring(0, 160)}...</p>
-                  <Votes id={article._id} votes={article.votes} type='articles' />
-                </li>
-              )
-            }
-          })}
-        </ul>
+        {this.state.loading ? <p>Loading...</p> :
+          <ul className='articles'>
+            {this.state.articles.map(article => {
+              let date = new Date(article.created_at)
+              let day = date.getDay()
+              let month = date.getMonth()
+              let year = date.getFullYear()
+              {
+                if (this.state.loading) return <p>Loading...</p>
+                return (
+                  <li key={article._id}>
+                    <Link to={`/articles/${article._id}`}>{article.title}</Link>
+                    <p>by {article.created_by.name}</p>
+                    <p>Posted on: {day}/{month}/{year}</p>
+                    <p>{article.body.substring(0, 160)}...</p>
+                    <Votes id={article._id} votes={article.votes} type='articles' />
+                  </li>
+                )
+              }
+            })}
+          </ul>}
       </div>
     );
   }
