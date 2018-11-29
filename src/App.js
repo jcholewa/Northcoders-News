6a176343057874fbc962732f59f12eae935d60f2
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-29 09:07:06 +0000

    Reroute users/:username path to Home rather than User

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
               <Home path='/articles' user={this.state.user} />
               <Home path='/topics/:topic_slug/articles' />
               <Article path='/articles/:article_id/*' user={this.state.user} />
-              <User path='/users/:username' />
+              <Home path='/users/:username' />
             </Router>
           </Login>
         </div>
