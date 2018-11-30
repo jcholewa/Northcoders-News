14aa760bdb76f41b3dc8ed4720491afe52a3806d
Joanna Cholewa <34255500+jcholewa@users.noreply.github.com>
2018-11-30 14:46:55 +0000

    Attempt to toggle vote buttons when clicked

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,74 +1,133 @@
-import { Link, navigate } from '@reach/router';
-import React, { Component } from 'react';
-import Votes from './Votes';
-import SortBy from './SortBy';
-import ArticleAdder from './ArticleAdder';
-import Loading from './Loading';
-import { postArticle, deleteItem } from '../api';
-import { getDate } from '../utils';
-import { sortBy } from 'underscore';
+import { Link, navigate } from "@reach/router";
+import React, { Component } from "react";
+import Votes from "./Votes";
+import SortBy from "./SortBy";
+import ArticleAdder from "./ArticleAdder";
+import Loading from "./Loading";
+import { postArticle, deleteItem } from "../api";
+import { getDate } from "../utils";
+import { sortBy } from "underscore";
 
 class Articles extends Component {
   state = {
     articles: [],
     loading: true,
-    article: '',
-    title: '',
-    topic: '',
-    sortBy: '',
+    article: "",
+    title: "",
+    topic: "",
+    sortBy: "",
     addArticle: false,
-    searchTerm: ''
-  }
+    searchTerm: ""
+  };
 
   render() {
-    const articles = this.state.articles.filter(article => article.body.toLowerCase().includes(this.state.searchTerm.toLowerCase()) || article.title.toLowerCase().includes(this.state.searchTerm.toLowerCase())) || this.state.articles;
+    const articles =
+      this.state.articles.filter(
+        article =>
+          article.body
+            .toLowerCase()
+            .includes(this.state.searchTerm.toLowerCase()) ||
+          article.title
+            .toLowerCase()
+            .includes(this.state.searchTerm.toLowerCase())
+      ) || this.state.articles;
 
-    if (this.state.loading) return <Loading />
-    if (this.state.err) return <p>{this.state.err}</p>
+    if (this.state.loading) return <Loading />;
+    if (this.state.err) return <p>{this.state.err}</p>;
 
     return (
       <div>
         <>
-
-          {this.state.addArticle ?
-            <ArticleAdder topic={this.state.topic} handleSubmit={this.handleSubmit} handleChange={this.handleChange} showArticleAdder={this.showArticleAdder} changeTopic={this.changeTopic} handleChangeTitle={this.handleChangeTitle} /> :
+          {this.state.addArticle ? (
+            <ArticleAdder
+              topic={this.state.topic}
+              handleSubmit={this.handleSubmit}
+              handleChange={this.handleChange}
+              showArticleAdder={this.showArticleAdder}
+              changeTopic={this.changeTopic}
+              handleChangeTitle={this.handleChangeTitle}
+            />
+          ) : (
             <>
-              <div className='searchBox'><input className='searchBar' type='text' placeholder='Search for articles...' onChange={this.onSearchChange} value={this.state.searchTerm} /><button onClick={this.onSearchSubmit}>Search</button></div><br />
-              <button className='add-article-button' onClick={this.showArticleAdder}>Add an article</button>
-              <SortBy handleChangeSort={this.handleChangeSort} value={'articles'} />
-              <ul className='articles'>
+              <div className="searchBox">
+                <input
+                  className="searchBar"
+                  type="text"
+                  placeholder="Search for articles..."
+                  onChange={this.onSearchChange}
+                  value={this.state.searchTerm}
+                />
+                <button onClick={this.onSearchSubmit}>Search</button>
+              </div>
+              <br />
+              <button
+                className="add-article-button"
+                onClick={this.showArticleAdder}
+              >
+                Add an article
+              </button>
+              <SortBy
+                handleChangeSort={this.handleChangeSort}
+                value={"articles"}
+              />
+              <ul className="articles">
                 {articles.map(article => {
-                  let dayPosted = getDate(article.created_at)
+                  let dayPosted = getDate(article.created_at);
                   {
-                    if (this.state.loading) return <p>Loading...</p>
+                    if (this.state.loading) return <p>Loading...</p>;
                     return (
                       <li key={article._id}>
-                        <Link to={`/articles/${article._id}`}>{article.title}</Link>
-                        {' by'} <Link to={`/users/${article.created_by.username}`}> {article.created_by.username}</Link> {' on '}{dayPosted} <br />
+                        <Link to={`/articles/${article._id}`}>
+                          {article.title}
+                        </Link>
+                        {" by"}{" "}
+                        <Link to={`/users/${article.created_by.username}`}>
+                          {" "}
+                          {article.created_by.username}
+                        </Link>{" "}
+                        {" on "}
+                        {dayPosted} <br />
                         <p>Topic: {article.belongs_to}</p>
                         <p>
                           <p>{article.body.substring(0, 160)}...</p>
-                          {article.comment_count} {article.comment_count === 1 ? ' comment' : ' comments'}
-                          <Votes id={article._id} votes={article.votes} type='articles' /></p>
-
-                        {(article.created_by.username === this.props.user.username) && <button onClick={(() => this.handleDelete(article._id))}>Delete article</button>}
+                          {article.comment_count}{" "}
+                          {article.comment_count === 1
+                            ? " comment"
+                            : " comments"}
+                          <Votes
+                            id={article._id}
+                            votes={article.votes}
+                            type="articles"
+                          />
+                        </p>
+                        {article.created_by.username ===
+                          this.props.user.username && (
+                          <button
+                            onClick={() => this.handleDelete(article._id)}
+                          >
+                            Delete article
+                          </button>
+                        )}
                       </li>
-                    )
+                    );
                   }
                 })}
               </ul>
-              <a className='back-to-top' href='#top'>Back to Top</a>
-            </>}
+              <a className="back-to-top" href="#top">
+                Back to Top
+              </a>
+            </>
+          )}
         </>
       </div>
-    )
+    );
   }
 
   componentDidMount() {
     this.setState({
       loading: false,
       articles: this.props.articles
-    })
+    });
   }
 
   componentDidUpdate(prevProps) {
@@ -76,96 +135,106 @@ class Articles extends Component {
       this.setState({
         loading: false,
         articles: this.props.articles
-      })
+      });
     }
   }
 
   showArticleAdder = event => {
     event.preventDefault();
     this.setState(state => {
-      return { addArticle: state.addArticle === true ? false : true }
-    })
-  }
+      return { addArticle: state.addArticle === true ? false : true };
+    });
+  };
 
   onSearchChange = event => {
     this.setState({
-      searchTerm: event.target.value,
-    })
-  }
+      searchTerm: event.target.value
+    });
+  };
 
   onSearchSubmit = event => {
     event.preventDefault();
-
-  }
+  };
 
   handleChange = event => {
     this.setState({
       article: event.target.value
-    })
-  }
+    });
+  };
 
   handleChangeTitle = event => {
     this.setState({
       title: event.target.value
-    })
-  }
+    });
+  };
 
   changeTopic = event => {
     this.setState({
-      topic: event.target.value,
-    })
-  }
+      topic: event.target.value
+    });
+  };
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    if (this.state.title && this.state.article && this.state.topic !== '')
-      postArticle(this.state.title, this.state.article, this.state.topic, this.props.user._id)
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.article && this.state.topic !== "")
+      postArticle(
+        this.state.title,
+        this.state.article,
+        this.state.topic,
+        this.props.user._id
+      )
         .then(article => {
           this.setState(state => {
-            return { articles: [article, ...state.articles] }
-          })
+            return { articles: [article, ...state.articles] };
+          });
         })
         .then(() => {
           this.setState({
-            article: '',
-            title: '',
+            article: "",
+            title: "",
             topic: null,
             addArticle: false
-          })
+          });
         })
         .catch(err => {
-          navigate('/error', {
-            replace: true, state: {
-              code: err.response.status,
+          navigate("/error", {
+            replace: true,
+            state: {
+              code: err.response.status
             }
-          })
-        })
-  }
+          });
+        });
+  };
 
   // This will be the onChange for the select element:
   handleChangeSort = event => {
-    const sortByValue = event.target.value
+    const sortByValue = event.target.value;
 
     this.setState(state => {
       return {
-        articles: sortByValue.includes('votes') ? sortByValue === 'votes-desc' ? sortBy(state.articles, 'votes').reverse() : sortBy(state.articles, 'votes') : sortByValue === 'time-desc' ? sortBy(state.articles, 'created_at').reverse() : sortBy(state.articles, 'created_at')
-      }
-    })
-  }
+        articles: sortByValue.includes("votes")
+          ? sortByValue === "votes-desc"
+            ? sortBy(state.articles, "votes").reverse()
+            : sortBy(state.articles, "votes")
+          : sortByValue === "time-desc"
+          ? sortBy(state.articles, "created_at").reverse()
+          : sortBy(state.articles, "created_at")
+      };
+    });
+  };
 
-  handleDelete = (id) => {
-    deleteItem(id, 'articles')
+  handleDelete = id => {
+    deleteItem(id, "articles")
       .then(message => {
         this.setState({
           articles: this.state.articles.filter(article => article._id !== id)
-        })
+        });
       })
       .then(() => {
-        window.alert('Article deleted!')
+        window.alert("Article deleted!");
       })
-      .catch(console.log)
-  }
-
+      .catch(console.log);
+  };
 }
 
 export default Articles;
--- src/Components/Votes.jsx
+++ src/Components/Votes.jsx
@@ -4,7 +4,9 @@ import { alterVoteMod } from '../utils';
 
 class Votes extends Component {
   state = {
-    voteMod: 0
+    voteMod: 0,
+    up: false,
+    down: false
   }
 
   render() {
@@ -12,13 +14,24 @@ class Votes extends Component {
       this.state.err ? <p>Something went wrong</p> :
         <div className='votes'>
           {this.props.votes + this.state.voteMod} {this.props.votes === 1 ? ' vote ' : ' votes '}
-          <button onClick={(() => this.handleVote('up'))} disabled={this.state.voteMod === 1} ><i className="far fa-thumbs-up"></i></button>
-          <button onClick={(() => this.handleVote('down'))} disabled={this.state.voteMod === -1}><i className="far fa-thumbs-down"></i></button>
+          <button className={this.state.up && 'up'} onClick={(() => this.handleVote('up'))} disabled={this.state.voteMod === 1} ><i className="far fa-thumbs-up"></i></button>
+          <button  onClick={(() => this.handleVote('down'))} disabled={this.state.voteMod === -1}><i className="far fa-thumbs-down"></i></button>
         </div>
     );
   }
 
   handleVote = (direction) => {
+    // if (direction === 'up') {
+    //   this.setState(state => {
+    //     up: state.up === true ? false : true
+    //   })
+    // }
+      
+      // up.classList.toggle('up')
+      // toggle green
+    // } else if (direction === 'down') {
+    //   // toggle red
+    // }
     alterVotes(this.props.id, direction, this.props.type)
       .catch(err => { this.setState({ err }) })
     this.setState(state => {
